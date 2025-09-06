8a2ddf1d57d1f82ffdbf17728b78e45c7637925a
Ziel Zonneveld <zonneveld.ziel@gmail.com>
2025-09-06 14:25:36 +0200

    Updated Lightbox and sitemaps

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://abr3havens.com',
+  site: "https://abr3havens.co.za",
 
   vite: {
     plugins: [tailwindcss()],
